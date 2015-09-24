462329f5514e4dfad82840fba16cc626adc24b29
James Curley <jc3181@columbia.edu>
2015-09-24 16:03:59 -0400

    fixed expandrows
    fixed expandrows

--- R/expandrows.R
+++ R/expandrows.R
@@ -1,6 +1,6 @@
 #' Expand rows of google forms
 #'
-#' @param df A dataframe
+#' @param df A dataframe with two rows "Actor" "Recipient" - have to be only two
 #' @return The dataframe with expanded rows, as well as a score variable.
 #' @examples
 #' expandrows(df)
@@ -15,31 +15,31 @@
 
   
 expandrows <- function(df){
-    
-    toDrop <- Actor <- Recipient <- id <- score <- NULL  ## not sure why this is needed
-    
-    temp <- cSplit(cSplit(cbind(id = 1:nrow(df), df),
-                          "Actor", ",", "long"), 
-                   "Recipient", ",", "long")
-    
-    
-    ## Convert "Actor" and "Recipient" to numeric
-    SD <- c("Actor", "Recipient")
-    temp[, (SD) := lapply(.SD, as.numeric), .SDcols = SD]
-    
-    
-    ## Sort Actors and Recipients, and check for duplicates and any points where Actors equal Recipients  
-    temp[, toDrop := duplicated(
-      paste(pmin(Actor, Recipient), pmax(Actor, Recipient))) |
-        Actor == Recipient, by = id]
-    
-    ## Create your "score" column
-    temp[, score := ifelse(any(toDrop), 0.5, 1), by = id]
-    
-    ## Subset and drop the irrelevant columns
-    out <- temp[!temp[, toDrop, with = TRUE]][, toDrop := NULL]
-    
-    return(out)
-  }
-
   
+  library(splitstackshape)  
+  library(data.table)  
+  
+  
+  temp <- cSplit(cSplit(cbind(id = 1:nrow(df), df),
+                        "Actor", ",", "long"), 
+                 "Recipient", ",", "long")
+  
+  
+  ## Convert "Actor" and "Recipient" to numeric
+  SD <- c("Actor", "Recipient")
+  temp[, (SD) := lapply(.SD, as.numeric), .SDcols = SD]
+  
+  
+  ## Sort Actors and Recipients, and check for duplicates and any points where Actors equal Recipients  
+  temp[, toDrop := duplicated(
+    paste(pmin(Actor, Recipient), pmax(Actor, Recipient))) |
+      Actor == Recipient, by = id]
+  
+  ## Create your "score" column
+  temp[, score := ifelse(any(toDrop), 0.5, 1), by = id]
+  
+  ## Subset and drop the irrelevant columns
+  temp <-  temp[temp$toDrop!=T,]
+  out <- temp[, c(2:3,5), with = FALSE]
+  return(out)
+}
